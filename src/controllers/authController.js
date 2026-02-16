9b569fc77d0097e9326a5a3769bc6a4f6f7c6bc4
apeiron888 <aelafgetaneh@gmail.com>
2026-02-16 20:58:00 +0300

    feat: enhance GitHub OAuth flow to resolve groupSheetId from group name

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const Student = require('../models/Student');
+const GroupSheet = require('../models/GroupSheet');
 const { encrypt } = require('../utils/encryption');
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
@@ -7,17 +8,35 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const CALLBACK_URL = process.env.GITHUB_CALLBACK_URL;
 
 // Redirect user to GitHub for authorization
-exports.githubAuth = (req, res) => {
-  const { email, groupSheetId, extensionId } = req.query;
-  if (!email || !groupSheetId || !extensionId) {
-    return res.status(400).send('Missing email, groupSheetId, or extensionId');
+exports.githubAuth = async (req, res) => {
+  try {
+    const { email, groupSheetId: rawGroupSheetId, groupName, extensionId } = req.query;
+    if (!email || !extensionId || (!rawGroupSheetId && !groupName)) {
+      return res.status(400).send('Missing email, group name (or sheet id), or extensionId');
+    }
+
+    let groupSheetId = rawGroupSheetId;
+    let resolvedGroupName = groupName;
+
+    if (!groupSheetId && groupName) {
+      const escaped = groupName.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+      const group = await GroupSheet.findOne({ name: new RegExp(`^${escaped}$`, 'i') });
+      if (!group) {
+        return res.status(404).send('Group name not found');
+      }
+      groupSheetId = group.sheetId;
+      resolvedGroupName = group.name || groupName;
+    }
+
+    // Store data in state to retrieve later
+    const state = Buffer.from(JSON.stringify({ email, groupSheetId, groupName: resolvedGroupName, extensionId })).toString('base64');
+
+    const url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${CALLBACK_URL}&scope=repo&state=${state}`;
+    res.redirect(url);
+  } catch (error) {
+    console.error('GitHub OAuth init error:', error);
+    res.status(500).send('Internal server error');
   }
-
-  // Store data in state to retrieve later
-  const state = Buffer.from(JSON.stringify({ email, groupSheetId, extensionId })).toString('base64');
-
-  const url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${CALLBACK_URL}&scope=repo&state=${state}`;
-  res.redirect(url);
 };
 
 // Handle callback
@@ -27,14 +46,28 @@ exports.githubCallback = async (req, res) => {
     return res.status(400).send('Missing code or state');
   }
 
-  let email, groupSheetId, extensionId;
+  let email, groupSheetId, groupName, extensionId;
   try {
     const stateData = JSON.parse(Buffer.from(state, 'base64').toString());
     email = stateData.email;
     groupSheetId = stateData.groupSheetId;
+    groupName = stateData.groupName;
     extensionId = stateData.extensionId;
   } catch (err) {
     return res.status(400).send('Invalid state');
+  }
+
+  if (!groupSheetId && groupName) {
+    const escaped = groupName.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const group = await GroupSheet.findOne({ name: new RegExp(`^${escaped}$`, 'i') });
+    if (!group) {
+      return res.status(404).send('Group name not found');
+    }
+    groupSheetId = group.sheetId;
+  }
+
+  if (!groupSheetId) {
+    return res.status(400).send('Missing group sheet id');
   }
 
   try {
